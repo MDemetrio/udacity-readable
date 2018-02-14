71cafb5267ac6a93717c5139e156db68d5e3c20f
MDemetrio <mdemetrio.dev@gmail.com>
2018-02-14 11:39:50 -0200

    feat: Create actions/reducers for Posts and Comments POST and PUT methods

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,11 @@
 import { CALL_API } from '../utils/api'
 
+//CATEGORIES
 export const GET_CATEGORIES_REQUEST = 'GET_CATEGORIES_REQUEST'
 export const GET_CATEGORIES_SUCCESS = 'GET_CATEGORIES_SUCCESS'
 export const GET_CATEGORIES_FAILURE = 'GET_CATEGORIES_FAILURE'
 
+//POSTS
 export const GET_POSTS_REQUEST = 'GET_POSTS_REQUEST'
 export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
 export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE'
@@ -12,19 +14,44 @@ export const GET_POST_REQUEST = 'GET_POST_REQUEST'
 export const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
 export const GET_POST_FAILURE = 'GET_POST_FAILURE'
 
-export const POST_POSTVOTE_REQUEST = 'POST_POSTS_REQUEST'
-export const POST_POSTVOTE_SUCCESS = 'POST_POSTS_SUCCESS'
-export const POST_POSTVOTE_FAILURE = 'POST_POSTS_FAILURE'
+export const POST_POST_REQUEST = 'POST_POST_REQUEST'
+export const POST_POST_SUCCESS = 'POST_POST_SUCCESS'
+export const POST_POST_FAILURE = 'POST_POST_FAILURE'
+
+export const PUT_POST_REQUEST = 'PUT_POST_REQUEST'
+export const PUT_POST_SUCCESS = 'PUT_POST_SUCCESS'
+export const PUT_POST_FAILURE = 'PUT_POST_FAILURE'
+
+export const POST_POSTVOTE_REQUEST = 'POST_POSTVOTE_REQUEST'
+export const POST_POSTVOTE_SUCCESS = 'POST_POSTVOTE_SUCCESS'
+export const POST_POSTVOTE_FAILURE = 'POST_POSTVOTE_FAILURE'
 
 export const DELETE_POSTS_REQUEST = 'DELETE_POSTS_REQUEST'
 export const DELETE_POSTS_SUCCESS = 'DELETE_POSTS_SUCCESS'
 export const DELETE_POSTS_FAILURE = 'DELETE_POSTS_FAILURE'
 
+//COMMENTS
 export const GET_COMMENTS_REQUEST = 'GET_COMMENTS_REQUEST'
 export const GET_COMMENTS_SUCCESS = 'GET_COMMENTS_SUCCESS'
 export const GET_COMMENTS_FAILURE = 'GET_COMMENTS_FAILURE'
 
-export const COMMENTS_SUCCESS = 'COMMENTS_SUCCESS'
+export const POST_COMMENT_REQUEST = 'POST_COMMENT_REQUEST'
+export const POST_COMMENT_SUCCESS = 'POST_COMMENT_SUCCESS'
+export const POST_COMMENT_FAILURE = 'POST_COMMENT_FAILURE'
+
+export const PUT_COMMENT_REQUEST = 'PUT_COMMENT_REQUEST'
+export const PUT_COMMENT_SUCCESS = 'PUT_COMMENT_SUCCESS'
+export const PUT_COMMENT_FAILURE = 'PUT_COMMENT_FAILURE'
+
+export const POST_COMMENTVOTE_REQUEST = 'POST_COMMENTVOTE_REQUEST'
+export const POST_COMMENTVOTE_SUCCESS = 'POST_COMMENTVOTE_SUCCESS'
+export const POST_COMMENTVOTE_FAILURE = 'POST_COMMENTVOTE_FAILURE'
+
+export const DELETE_COMMENT_REQUEST = 'DELETE_COMMENT_REQUEST'
+export const DELETE_COMMENT_SUCCESS = 'DELETE_COMMENT_SUCCESS'
+export const DELETE_COMMENT_FAILURE = 'DELETE_COMMENT_FAILURE'
+
+
 
 export const fetchCategories = () => {
     return ({
@@ -81,7 +108,7 @@ export const postVote = (id, option) => {
             types: [POST_POSTVOTE_REQUEST, POST_POSTVOTE_SUCCESS, POST_POSTVOTE_FAILURE],
             endpoint: `/posts/${id}`,
             method: 'POST',
-            body: {option}
+            body: { option }
         }
     })
 }
@@ -92,6 +119,71 @@ export const deletePost = postId => {
             types: [DELETE_POSTS_REQUEST, DELETE_POSTS_SUCCESS, DELETE_POSTS_FAILURE],
             endpoint: `/posts/${postId}`,
             method: 'DELETE'
+        }
+    })
+}
+
+export const deleteComment = commentId => {
+    return ({
+        [CALL_API]: {
+            types: [DELETE_COMMENT_REQUEST, DELETE_COMMENT_SUCCESS, DELETE_COMMENT_FAILURE],
+            endpoint: `/comments/${commentId}`,
+            method: 'DELETE'
+        }
+    })
+}
+
+export const commentVote = (id, option) => {
+    return ({
+        [CALL_API]: {
+            types: [POST_COMMENTVOTE_REQUEST, POST_COMMENTVOTE_SUCCESS, POST_COMMENTVOTE_FAILURE],
+            endpoint: `/comments/${id}`,
+            method: 'POST',
+            body: { option }
+        }
+    })
+}
+
+export const postComment = (comment) => {
+    return ({
+        [CALL_API]: {
+            types: [POST_COMMENT_REQUEST, POST_COMMENT_SUCCESS, POST_COMMENT_FAILURE],
+            endpoint: `/comments`,
+            method: 'POST',
+            body: comment
+        }
+    })
+}
+
+export const putComment = (commentId, comment) => {
+    return ({
+        [CALL_API]: {
+            types: [PUT_COMMENT_REQUEST, PUT_COMMENT_SUCCESS, PUT_COMMENT_FAILURE],
+            endpoint: `/comments/${commentId}`,
+            method: 'PUT',
+            body: comment
+        }
+    })
+}
+
+export const postPost = (post) => {
+    return ({
+        [CALL_API]: {
+            types: [POST_POST_REQUEST, POST_POST_SUCCESS, POST_POST_FAILURE],
+            endpoint: `/posts`,
+            method: 'POST',
+            body: post
+        }
+    })
+}
+
+export const putPost = (postId, post) => {
+    return ({
+        [CALL_API]: {
+            types: [PUT_POST_REQUEST, PUT_POST_SUCCESS, PUT_POST_FAILURE],
+            endpoint: `/posts/${postId}`,
+            method: 'PUT',
+            body: post
         }
     })
 }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -32,7 +32,13 @@ const posts = (state = { postsList: [], selectedPost: { comments: [] } }, action
         case ActionTypes.GET_POST_REQUEST:
         case ActionTypes.GET_COMMENTS_REQUEST:
         case ActionTypes.POST_POSTVOTE_REQUEST:
+        case ActionTypes.POST_COMMENTVOTE_REQUEST:
         case ActionTypes.DELETE_POSTS_REQUEST:
+        case ActionTypes.DELETE_COMMENT_REQUEST:
+        case ActionTypes.POST_POST_REQUEST:
+        case ActionTypes.PUT_POST_REQUEST:
+        case ActionTypes.POST_COMMENT_REQUEST:
+        case ActionTypes.PUT_COMMENT_REQUEST:
             return {
                 ...state,
                 isFetching: true
@@ -41,7 +47,13 @@ const posts = (state = { postsList: [], selectedPost: { comments: [] } }, action
         case ActionTypes.GET_POST_FAILURE:
         case ActionTypes.GET_COMMENTS_FAILURE:
         case ActionTypes.POST_POSTVOTE_FAILURE:
+        case ActionTypes.POST_COMMENTVOTE_FAILURE:
         case ActionTypes.DELETE_POSTS_FAILURE:
+        case ActionTypes.DELETE_COMMENT_FAILURE:
+        case ActionTypes.POST_POST_FAILURE:
+        case ActionTypes.PUT_POST_FAILURE:
+        case ActionTypes.POST_COMMENT_FAILURE:
+        case ActionTypes.PUT_COMMENT_FAILURE:
             return {
                 ...state,
                 error: action.error,
@@ -68,18 +80,55 @@ const posts = (state = { postsList: [], selectedPost: { comments: [] } }, action
                     comments: action.response
                 }
             }
-        case ActionTypes.POST_POSTVOTE_SUCCESS:
-            return {
-                ...state,
-                isFetching: false,
-                postsList: updateObjectInArray(state.postsList, action),
-                selectedPost: action.response
-            }
         case ActionTypes.DELETE_POSTS_SUCCESS:
             return {
                 ...state,
                 isFetching: false,
                 postsList: state.postsList.filter((item, index) => item.id !== action.response.id)
+            }
+        case ActionTypes.DELETE_COMMENT_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                selectedPost: {
+                    ...state.selectedPost,
+                    commentCount: state.selectedPost.commentCount -= 1,
+                    comments: state.selectedPost.comments.filter((item, index) => item.id !== action.response.id)
+                }
+            }
+        case ActionTypes.POST_POST_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                postsList: [...state.postsList, action.response]
+            }
+        case ActionTypes.POST_COMMENT_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                selectedPost: {
+                    ...state.selectedPost,
+                    commentCount: state.selectedPost.commentCount += 1,
+                    comments: [...state.selectedPost.comments, action.response]
+                }
+            }
+        case ActionTypes.POST_POSTVOTE_SUCCESS:
+        case ActionTypes.PUT_POST_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                postsList: updateObjectInArray(state.postsList, action.response),
+                selectedPost: { ...state.selectedPost, ...action.response }
+            }
+        case ActionTypes.PUT_COMMENT_SUCCESS:
+        case ActionTypes.POST_COMMENTVOTE_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                selectedPost: {
+                    ...state.selectedPost,
+                    comments: updateObjectInArray(state.selectedPost.comments, action.response)
+                }
             }
         default:
             return state;
