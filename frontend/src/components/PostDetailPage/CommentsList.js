47a0d0c9b947d221ec747b22d223d7b76dba17ca
MDemetrio <mdemetrio.dev@gmail.com>
2018-02-14 11:41:42 -0200

    feat: Add style and functionalities to CommentsList component

--- frontend/src/components/PostDetailPage/CommentsList.js
+++ frontend/src/components/PostDetailPage/CommentsList.js
@@ -2,17 +2,112 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { orderArray } from "../../utils/helpers";
+import MdClose from 'react-icons/lib/md/close';
+import MdEdit from 'react-icons/lib/md/edit';
+import { IconButton, UpVoteIcon, DownVoteIcon, FooterContainer, FooterItemContainer, BodyContainer } from "../shared";
+import { deleteComment, commentVote, postComment, putComment } from "../../actions";
+import CommentForm from "./CommentForm";
+import { guid } from "../../utils/helpers";
+import Button from 'muicss/lib/react/button';
+import Panel from 'muicss/lib/react/panel';
 
 class CommentsList extends Component {
+    state = {
+        commentEdit: {
+            editing: false,
+            commentId: ""
+        }
+    }
+
+    voteHandler = (e, postId, vote) => {
+        e.preventDefault();
+        this.props.commentVote(postId, vote);
+    }
+
+    excludeHandler = (e, postId) => {
+        e.preventDefault();
+        this.props.deleteComment(postId).then(
+            this.setState({ deleted: true })
+        );
+    }
+
+    editHandler = (e, commentId) => {
+        e.preventDefault();
+        this.setState(prevState => {
+            const editing = prevState.commentEdit.commentId === commentId ? !prevState.commentEdit.editing : true
+            return { commentEdit: { commentId, editing } }
+        })
+    }
+
+    submitComment = (comment, editMode = false) => {
+        if (!editMode) {
+            comment.id = guid();
+            this.props.postComment(comment);
+        }
+        else
+            this.props.putComment(comment.id, comment);
+
+        this.setState({
+            commentEdit: {
+                editing: false,
+                commentId: ""
+            }
+        })
+    }
+
     render() {
-        const { postComments } = this.props;
+        const { postComments, postId } = this.props;
+        const { commentEdit } = this.state;
+
+        const comments = orderArray(postComments, { field: 'voteScore', ascending: true }).map(comment => ({
+            ...comment,
+            createdAt: new Date(comment.timestamp),
+            editing: false,
+        }));
+
         return (
-            <div style={{'backgroundColor': 'lightGrey'}}>
+            <div>
+                <Button variant="raised" onClick={(e) => this.editHandler(e, "")}>New Comment</Button>
+                {
+                    (commentEdit.commentId === "" && commentEdit.editing) &&
+                    <CommentForm onSubmit={this.submitComment} initialData={{ parentId: postId }} />
+                }
                 <ul>
-                    {postComments.map(c => (
-                        <div key={c.id}>
-                            {c.id}
-                        </div>
+                    {comments.map(c => (
+                        <Panel key={c.id}>
+                            <BodyContainer>{c.body}</BodyContainer>
+
+                            <FooterContainer>
+                                <div>{c.author}, {c.createdAt.toDateString()}</div>
+
+                                <FooterItemContainer>
+                                    {c.voteScore}
+
+                                    <IconButton onClick={(e) => this.voteHandler(e, c.id, "upVote")}>
+                                        <UpVoteIcon />
+                                    </IconButton>
+
+                                    <IconButton onClick={(e) => this.voteHandler(e, c.id, "downVote")}>
+                                        <DownVoteIcon />
+                                    </IconButton>
+                                </FooterItemContainer>
+
+                                <FooterItemContainer>
+                                    <IconButton onClick={(e) => this.editHandler(e, c.id)}>
+                                        <MdEdit />
+                                    </IconButton>
+
+                                    <IconButton onClick={(e) => this.excludeHandler(e, c.id)}>
+                                        <MdClose />
+                                    </IconButton>
+                                </FooterItemContainer>
+                            </FooterContainer>
+                            {
+                                (commentEdit.commentId === c.id && commentEdit.editing) &&
+                                <CommentForm onSubmit={this.submitComment} initialData={c} editMode />
+                            }
+                        </Panel>
                     ))}
                 </ul>
             </div>
@@ -21,11 +116,16 @@ class CommentsList extends Component {
 }
 
 CommentsList.propTypes = {
-    postComments: PropTypes.array.isRequired
+    postComments: PropTypes.array.isRequired,
+    postId: PropTypes.string.isRequired
 }
 
 function mapDispatchToProps(dispatch) {
     return {
+        commentVote: (commentId, option) => dispatch(commentVote(commentId, option)),
+        deleteComment: commentId => dispatch(deleteComment(commentId)),
+        postComment: comment => dispatch(postComment(comment)),
+        putComment: (commentId, comment) => dispatch(putComment(commentId, comment))
     }
 }
 
--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,6 +1,6 @@
-export function updateObjectInArray(array, action) {
+export function updateObjectInArray(array, obj) {
     return array.map((item, index) => {
-        if (item.id !== action.response.id) {
+        if (item.id !== obj.id) {
             // This isn't the item we care about - keep it as-is
             return item;
         }
@@ -8,7 +8,27 @@ export function updateObjectInArray(array, action) {
         // Otherwise, this is the one we want - return an updated value
         return {
             ...item,
-            ...action.response
+            ...obj
         };
     });
 }
+
+export function orderArray(arr, orderBy = { field: '', ascending: true }) {
+    return arr.sort((a, b) => {
+        const result = a[orderBy.field] > b[orderBy.field];
+        if (orderBy.ascending)
+            return result ? -1 : 1;
+        else
+            return result ? 1 : -1;
+    });
+}
+
+export function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
